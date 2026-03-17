a316c798a7f1d531d9d505bbb7b2b6e89579e526
ronak-ews <ronaksheikh2@gmail.com>
2026-03-17 17:50:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// smooth scrolling for navigation links
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
